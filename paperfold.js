c646971519730be17395a13f14147aadb572260a
Umut Zeytindalı <umutzeytindali@outlook.com>
2023-08-27 04:36:05 +0300

    Add files

---
+++ paperfold.js
@@ -0,0 +1,27 @@
+function fold() {
+  let val = document.getElementById("times").value;
+  let result = document.getElementById("result");
+  let foldResult = 0.01;
+  for (let i = 1; i <= val; i++) {
+    foldResult *= 2;
+  }
+  if (foldResult >= 990352031428304175103) {
+    result.innerHTML = "too damn";
+  } else if (foldResult >= 100000) {
+    result.innerHTML = (foldResult / 100000).toFixed(2) + "km";
+  } else if (foldResult >= 100) {
+    result.innerHTML = (foldResult / 100).toFixed(2) + "m";
+  } else {
+    result.innerHTML = foldResult + "cm";
+  }
+}
+
+function whyNot() {
+  let why = document.getElementById("why");
+
+  if (why.innerText == "Why?") {
+    why.innerText = "Why Not?";
+  } else if (why.innerText == "Why Not?") {
+    why.innerText = "Why?";
+  }
+}
